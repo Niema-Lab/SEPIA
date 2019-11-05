e18f0c6d21023c93ed06c5eb848c8923f0d04bea
mckennalewis11 <m2lewis@ucsd.edu>
2019-11-05 11:55:07 -0800

    all files

---
+++ testParse.py
@@ -0,0 +1,23 @@
+# Open this file. specify a file from user args
+f = open("01.practiceTransmission.txt", "r")
+
+infectedPersons=[]
+people = []
+
+num_infected = dict()
+# Loop over each line in the file.
+for line in f.readlines():
+    u,v,t = line.split('\t')
+    u = u.strip()
+    v = v.strip()
+    lower_bound = 0
+    upper_bound = 9
+
+    # why does this not work?
+    #if lower_bound <= float(t) <= upper_bound:
+
+    if u not in num_infected:
+        num_infected[u] = 0
+    num_infected[u] += 1
+    print num_infected[u]
+
