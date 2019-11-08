07c58b3d4f6239a18c431d4605d6bcc81655386b
Titan-Ngo <ttngo@ucsd.edu>
2019-11-07 16:51:12 -0800

    added matchInfectorCounts

--- testOrderCheck.py
+++ testOrderCheck.py
@@ -2,6 +2,10 @@ from orderCheck import *
 
 infectionsDict = countInfections("01.transmissions.txt", 0, 5)
 
+userSortedInfectors = ['CNG0-COM0-92','CNG0-COM17-1425','CNG0-COM12-92','CNG0-COM11-190']
+
+userSortedCounts  = matchInfectorCounts(infectionsDict, userSortedInfectors)
+
 infectionsValues = infectionsDict.values()
 
-#calculateTauB(infectionsValues, userList)
+#calculateTauB(infectionsValues, userSortedCounts)
